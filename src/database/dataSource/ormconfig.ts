4d2bc7b1d3339fed850840a7c370f211c72c870d
민대인 <bamin0422@gmail.com>
2025-08-26 23:53:49 +0900

    Configures database connection settings
    Updates database configuration to use environment variables
    for connection details, falling back to default settings if not provided.
    Also, configures entities using direct imports instead of a path.
    This change enhances flexibility and maintainability by allowing
    database settings to be easily configured through environment variables.

--- src/database/dataSource/ormconfig.ts
+++ src/database/dataSource/ormconfig.ts
@@ -2,6 +2,10 @@ import dotenv from 'dotenv';
 import { DataSourceOptions } from 'typeorm';
 
 import { DEV_SETTING, PROD_SETTING } from '@constants/index';
+import Admin from '@database/entities/admin';
+import Post from '@database/entities/post';
+import Image from '@database/entities/image';
+import Comment from '@database/entities/comment';
 
 dotenv.config();
 
@@ -13,27 +17,27 @@ interface OrmconfigType {
 export const ormconfig: OrmconfigType = {
   dev: {
     type: 'mysql',
-    host: process.env.DEV_DB_HOST,
-    port: Number(DEV_SETTING.db.port),
-    username: process.env.DEV_DB_USERNAME,
-    password: process.env.DEV_DB_PASSWORD,
-    database: DEV_SETTING.db.database,
+    host: process.env.DB_HOST || DEV_SETTING.db.host,
+    port: Number(process.env.DB_PORT || DEV_SETTING.db.port),
+    username: process.env.DB_USERNAME || DEV_SETTING.db.username,
+    password: process.env.DB_PASSWORD || DEV_SETTING.db.password,
+    database: process.env.DB_NAME || DEV_SETTING.db.database,
     synchronize: true,
     logging: true,
-    entities: ['src/database/entities/**/*.ts'],
+    entities: [Admin, Post, Image, Comment],
     migrations: ['src/database/migration/**/*.ts'],
     subscribers: ['src/database/subscriber/**/*.ts'],
   },
   prod: {
     type: 'mysql',
-    host: process.env.DB_HOST,
-    port: Number(PROD_SETTING.db.port),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: PROD_SETTING.db.database,
+    host: process.env.DB_HOST || PROD_SETTING.db.host,
+    port: Number(process.env.DB_PORT || PROD_SETTING.db.port),
+    username: process.env.DB_USERNAME || PROD_SETTING.db.username,
+    password: process.env.DB_PASSWORD || PROD_SETTING.db.password,
+    database: process.env.DB_NAME || PROD_SETTING.db.database,
     synchronize: true,
     logging: false,
-    entities: ['src/database/entities/**/*.ts'],
+    entities: [Admin, Post, Image, Comment],
     migrations: ['src/database/migration/**/*.ts'],
     subscribers: ['src/database/subscriber/**/*.ts'],
   },
